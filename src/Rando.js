593998056e3ac10b41acdedefd3968fdcf24a3ae
Sunil Karki <karkisunil1200@gmail.com>
2019-06-09 15:11:07 -0500

    Random number generator

--- src/Rando.js
+++ src/Rando.js
@@ -3,14 +3,15 @@ import React, {Component} from 'react';
 class Rando extends Component {
   constructor(props) {
     super(props);
-    this.state = {num: ''};
+    this.state = {num: '', clicked: false};
   }
 
   clickHandler = event => {
     event.preventDefault();
     let number = Math.floor(Math.random() * 10) + 1;
     this.setState({
-      num: number
+      num: number,
+      clicked: true
     });
   };
 
@@ -18,10 +19,15 @@ class Rando extends Component {
     console.log(this.state);
     return (
       <div>
-        <h1>Number : {this.state.num}</h1>
-        <button onClick={this.clickHandler} type='button' className='btn btn-primary btn-lg'>
-          Add me
-        </button>
+        <h1 className='text-warning'>Play the Game and match the number</h1>
+        <h1 className='bg-danger p-3'>Number : {this.state.num}</h1>
+        {this.state.num === 4 ? (
+          <h2 className='text-success'>You win</h2>
+        ) : (
+          <button onClick={this.clickHandler} type='button' className='btn btn-primary btn-lg'>
+            Add me
+          </button>
+        )}
       </div>
     );
   }
